ec3105165e78f8d0b3799b4ded5e38c4b6eae837
Corentin Lingier <corentin.lingier@student.uclouvain.be>
2019-04-23 22:26:20 +0200

    Recherche par titre de recette + API recette par titre + DOCSTRING + log
    Implementation de la recherche par recette cote application flask_ask.
    
    Implementation de la requete API
    
    Docstring de toutes les fonctions.
    
    Log de la semaine

--- atable/api.py
+++ atable/api.py
@@ -6,6 +6,10 @@ from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
 
+MARMITTON_URL = 'https://www.marmiton.org/recettes/recherche.aspx?'
+
+__all__ = ["api_request", "recherche_par_ingredients", "recherche_par_titre"]
+
 
 def api_request(url: str) -> List[Dict[str, str]]:
     """Scrape les titres et liens des resultats de la recherche Marmitton.
@@ -21,8 +25,9 @@ def api_request(url: str) -> List[Dict[str, str]]:
     Return
     -------
     out: List[Dict[str, str]]
-        Liste de Dictionnaire, ou chaque dictionnaire est
-        une recette.
+        Liste de Dictionnaire, ou chaque dictionnaire est une recette.
+        format:
+            {'title': 'Spaghetti Bolognaise', 'url': 'url_vers_la_recette'}
     """
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -36,6 +41,26 @@ def api_request(url: str) -> List[Dict[str, str]]:
             'a', attrs={'class': 'recipe-card-link'}).attrs['href']
         resultats_recherche.append(dict_recette)
     return resultats_recherche
+
+
+def recherche_par_titre(titre: str) -> List[Dict[str, str]]:
+    """Cherche le titre de la recette sur Marmitton.
+
+    Arguments
+    ---------
+    titre: str
+        Nom de la recette a chercher.
+
+    Return
+    ------
+    out: List[Dict[str, str]]
+        Liste de dictionnaire ou chaque dictionnaire represente une recette.
+        format:
+            voir api_request()
+    """
+    encoded = urlencode({"aqt": titre, "st": 0})
+    results = api_request(MARMITTON_URL + encoded)
+    return results
 
 
 def recherche_par_ingredients(ingredients: List[str]) -> List[Dict[str, str]]:
@@ -61,9 +86,8 @@ def recherche_par_ingredients(ingredients: List[str]) -> List[Dict[str, str]]:
                 {'title': 'Lasagne', 'url': 'http://...'}
             ]
     """
-    url = 'https://www.marmiton.org/recettes/recherche.aspx?'
     encoded = urlencode({"aqt": "-".join(ingredients), "st": 1})
-    results = api_request(url + encoded)
+    results = api_request(MARMITTON_URL + encoded)
     return results
 
 
@@ -80,7 +104,7 @@ def get_recette_par_etape(recipe_url: str) -> Dict[str, List[str]]:
     recipe_dict: Dict[str, List[str]]
         Un dictionnaire de cette forme:
         recipe_dict = {
-            'ingredients': [..], 
+            'ingredients': [..],
             'etapes':[..]
         }
 
